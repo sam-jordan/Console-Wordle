d193744c28668b8ba8a92b46a2e6b12208fb8538
sam-jordan <samjjordan24@gmail.com>
2025-09-05 14:09:37 +0100

    Added more test instances to fix broken checkGameOver test

--- index.test.ts
+++ index.test.ts
@@ -21,23 +21,23 @@ describe('checkValidGuess function', () => {
 
     it('should throw an error when "12345" submitted', () => {
         expect(() => testGame.checkValidGuess('12345')).toThrow();
-    })
+    });
 
     it('should throw an error when "aaaaa" submitted', () => {
         expect(() => testGame.checkValidGuess('aaaaa')).toThrow();
-    })
+    });
 
     it('should throw an error when "nights" submitted', () => {
         expect(() => testGame.checkValidGuess('nights')).toThrow();
-    })
+    });
 
     it('should throw an error when "ape" submitted', () => {
         expect(() => testGame.checkValidGuess('ape')).toThrow();
-    })
+    });
 
     it('should throw an error when "smart" submitted', () => {
         expect(() => testGame.checkValidGuess('smart')).toThrow();
-    })
+    });
 });
 
 /**
@@ -51,24 +51,26 @@ describe('checkGameOver function', () => {
 
     it('should return 0 when guess number is 1', () => {
         expect(testGame.checkGameOver(1)).toBe(0);
-    })
+    });
 
     describe('checkGameOver game lost', () => {
-        testGame.guesses.push([
-            {character: 'S', colour: '\x1b[33m'}, 
-            {character: 'M', colour: '\x1b[33m'}, 
-            {character: 'A', colour: '\x1b[33m'},
-            {character: 'R', colour: '\x1b[33m'},
-            {character: 'T', colour: '\x1b[33m'}
+        const lostGame = new Game();
+        lostGame.guesses.push([
+            {character: 'S', colour: '\x1b[0m'}, 
+            {character: 'M', colour: '\x1b[0m'}, 
+            {character: 'A', colour: '\x1b[0m'},
+            {character: 'R', colour: '\x1b[0m'},
+            {character: 'T', colour: '\x1b[0m'}
         ]);
 
         it('should return 2 when guess number is 6 and the latest guess is incorrect', () => {
-            expect(testGame.checkGameOver(6)).toBe(2);
-        })
-    })
+            expect(lostGame.checkGameOver(6)).toBe(2);
+        });
+    });
 
     describe('checkGameOver game won', () => {
-        testGame.guesses.push([
+        const wonGame = new Game();
+        wonGame.guesses.push([
             {character: 'S', colour: '\x1b[32m'}, 
             {character: 'M', colour: '\x1b[32m'}, 
             {character: 'A', colour: '\x1b[32m'},
@@ -77,12 +79,12 @@ describe('checkGameOver function', () => {
         ]);
 
         it('should return 1 when guess number is 2 and the latest guess is correct', () => {
-            expect(testGame.checkGameOver(2)).toBe(1);
-        })
+            expect(wonGame.checkGameOver(2)).toBe(1);
+        });
 
         it('should return 1 when guess number is 6 and the latest guess is correct', () => {
-            expect(testGame.checkGameOver(6)).toBe(1);
-        })
+            expect(wonGame.checkGameOver(6)).toBe(1);
+        });
     });
 });
 
@@ -91,13 +93,13 @@ describe('getEndMessage function', () => {
     
     it('should return a win message when called with status code 1', () => {
         expect(testGame.getEndMessage(1, 3)).toBe(`The word was ${testGame.solution.join('')}! You got it in 3 guesses.`)
-    })
+    });
 
     it('should return a lose message when called with status code 2', () => {
         expect(testGame.getEndMessage(2, 3)).toBe(`So close! The word was ${testGame.solution.join('')}.`)
-    })
+    });
 
     it('should return invalid status code when called with 3', () => {
         expect(testGame.getEndMessage(3, 3)).toBe('Invalid status code.')
-    })
+    });
 });
